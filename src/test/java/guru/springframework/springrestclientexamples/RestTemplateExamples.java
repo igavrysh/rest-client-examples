88131d16d3be2eda07f7e93274706dafcabfef98
Ievgen Gavrysh <zhenya.ga@gmail.com>
2019-09-03 15:55:50 -0700

    Add delete for RestTemplate

--- src/test/java/guru/springframework/springrestclientexamples/RestTemplateExamples.java
+++ src/test/java/guru/springframework/springrestclientexamples/RestTemplateExamples.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
@@ -177,6 +178,37 @@ public class RestTemplateExamples {
         JsonNode updatedNode = restTemplate.patchForObject(apiUrl + id, entity, JsonNode.class);
 
         System.out.println(updatedNode.toString());
+    }
+
+    @Test(expected = HttpClientErrorException.class)
+    public void deleteCustomer() throws Exception {
+
+        // create customer to update
+        String apiUrl = API_ROOT + "/customers/";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("firstname", "Sam");
+        postMap.put("lastname", "Axe");
+
+        // Java object to parse to JSON
+        JsonNode jsonNode = restTemplate.postForObject(apiUrl, postMap, JsonNode.class);
+
+        System.out.println("Response");
+        System.out.println(jsonNode.toString());
+
+        String customerUrl = jsonNode.get("customer_url").textValue();
+        String id = customerUrl.split("/")[3];
+
+        System.out.println("Created customer id: " + id);
+
+        restTemplate.delete(apiUrl + id); // expects 200 status
+
+        System.out.println("Customer deleted");
+
+        // should go boom on 404
+        restTemplate.getForObject(apiUrl + id, JsonNode.class);
     }
 
 }
